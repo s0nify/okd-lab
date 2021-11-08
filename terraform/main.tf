dcf469f1c1ec009762ef5128814fafc546c03dc8
Sonify <givemeparachute@gmail.com>
2021-11-08 17:19:08 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,6 @@ terraform {
 # Set the variable value in *.tfvars file
 # or using -var="do_token=..." CLI option
 variable "do_token" {}
-variable "pvt_key" {}
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
