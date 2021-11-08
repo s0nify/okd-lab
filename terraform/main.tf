b7b71eae9935606320292ed3c83d30f866ec3734
Sonify <givemeparachute@gmail.com>
2021-11-09 00:10:26 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         openstack = {
             source = "terraform-provider-openstack/openstack"
-            version = "1.45.0"
+            version = "1.33.0"
         }
         
         mcs = {
