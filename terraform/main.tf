43e90d62e82e99d1650f05356428a11990c6d795
Sonify <givemeparachute@gmail.com>
2021-11-08 21:33:17 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,21 +24,13 @@ data "digitalocean_ssh_key" "terraform" {
   name = "terraform"
 }
 
-resource "digitalocean_droplet" "okd-terminal" {
+resource "digitalocean_droplet" "okd-services" {
   image  = "ubuntu-18-04-x64"
-  name   = "okd-terminal"
+  name   = "okd-services"
   region = "fra1"
   size   = "s-1vcpu-1gb"
   vpc_uuid = "4a6f166a-ebaa-48d9-bd31-29e49c678b71"
 }
-
-#resource "digitalocean_droplet" "okd-services" {
-#  image  = "ubuntu-18-04-x64"
-#  name   = "okd-services"
-#  region = "fra1"
-#  size   = "s-1vcpu-1gb"
-#  vpc_uuid = "4a6f166a-ebaa-48d9-bd31-29e49c678b71"
-#}
 
 resource "digitalocean_project_resources" "cloud-okd-lab" {
   project = data.digitalocean_project.cloud-okd-lab.id
