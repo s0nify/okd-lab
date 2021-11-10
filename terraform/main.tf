42cb3f9562b4757d5cfa0d21d2387da157629ee0
Sonify <givemeparachute@gmail.com>
2021-11-10 15:17:43 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,11 +95,11 @@ resource "openstack_networking_port_v2" "port" {
   admin_state_up     = "true"
   security_group_ids = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
 
-  fixed_ip {
-    count = "${var.number_of_workers + var.number_of_workers}" 
-    subnet_id  = "${openstack_networking_subnet_v2.subnet_1.id}"
-    ip_address = "192.168.199.${9 + 1}"  
-  }
+#  fixed_ip {
+#    count = "${var.number_of_workers + var.number_of_workers}" 
+#    subnet_id  = "${openstack_networking_subnet_v2.subnet_1.id}"
+#    ip_address = "192.168.199.${9 + 1}"  
+#  }
 }
 
 
