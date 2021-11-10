439016db2e076b0c019c9ad7dfe48c9b867965a6
Sonify <givemeparachute@gmail.com>
2021-11-10 19:22:31 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,7 +109,7 @@ resource "openstack_compute_instance_v2" "master" {
   image_name      = openstack_images_image_v2.fedoracore.name
   security_groups = ["${openstack_compute_secgroup_v2.secgroup_1.name}"]
   network {
-    port        = "${openstack_networking_port_v2.okd-master-port.*.id[count.index]}"
+#    port        = "${openstack_networking_port_v2.okd-master-port.*.id[count.index]}"
 	fixed_ip_v4 = "192.168.199.${9+1}"
   }
 }
