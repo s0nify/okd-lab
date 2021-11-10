dc82ba5e601a0fd52e74417af4dad4f601ddcfc0
Sonify <givemeparachute@gmail.com>
2021-11-10 13:56:48 +0300

    +

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,42 +30,19 @@ terraform {
 }
 
 provider "openstack" {
-    # Your user account.
     user_name = var.MCS_USERNAME
-
-    # The password of the account
     password = var.MCS_PASSWORD
-
-    # The tenant token can be taken from the project Settings tab - > API keys.
-    # Project ID will be our token.
     tenant_id = var.MCS_PROJECT_ID
-
-    # The indicator of the location of users.
     user_domain_id = "users"
-
-    # API endpoint
-    # Terraform will use this address to access the VK Cloud Solutions api.
     auth_url = "https://infra.mail.ru:35357/v3/"
-
-    # use octavia to manage load balancers
     use_octavia = true
-    
-    # Region name
     region = "RegionOne"
 }
 
 provider "mcs" {
-    # Your user account.
     username = "givemeparachute@gmail.com"
-
-    # The password of the account
     password = "YOUR_PASSWORD"
-
-    # The tenant token can be taken from the project Settings tab - > API keys.
-    # Project ID will be our token.
     project_id = "da50a396acf14358a85377d17e46b613"
-    
-    # Region name
     region = "RegionOne"
 }
 
