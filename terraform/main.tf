b9bd93c1aeed82488537b198b807f64e6c1f536a
Sonify <givemeparachute@gmail.com>
2021-11-10 20:54:57 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ resource "openstack_networking_port_v2" "okd-master-port" {
   admin_state_up     = "true"
 #  security_group_ids = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
 
-  fixed_ip [
+  fixed_ip = [
   {
     ip_address = 192.168.199.10
     subnet_id  = "${openstack_networking_subnet_v2.okd-subnet.id}"
