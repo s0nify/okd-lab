b09e070696181f52d6bdf9dadf9a78569ef08157
Sonify <givemeparachute@gmail.com>
2021-11-10 20:37:31 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,92 +29,33 @@ provider "mcs" {
     region = "RegionOne"
 }
 
-resource "openstack_images_image_v2" "fedoracore" {
-  name             = "Fedora CoreOS 34.20211016.3.0-openstack"
-  image_source_url = "https://repo.hb.bizmrg.com/fedora-coreos-34.20211031.3.0-openstack.x86_64.qcow2"
-  container_format = "bare"
-  disk_format      = "raw"
-}
-
-resource "openstack_compute_keypair_v2" "ssh" {
-  name = "terraform_ssh_key"
-  public_key = var.SSH_KEY
-}
-
-resource "openstack_compute_secgroup_v2" "rules" {
-  name = "terraform__security_group"
-  description = "security group for terraform instance"
-  rule {
-    from_port = 22
-    to_port = 22
-    ip_protocol = "tcp"
-    cidr = "0.0.0.0/0"
-  }
-  rule {
-    from_port = 80
-    to_port = 80
-    ip_protocol = "tcp"
-    cidr = "0.0.0.0/0"
-  }
-  rule {
-    from_port = -1
-    to_port = -1
-    ip_protocol = "icmp"
-    cidr = "0.0.0.0/0"
-  }
-}
-
-resource "openstack_networking_network_v2" "network_1" {
-  name           = "int-okd-lab-1"
+resource "openstack_networking_network_v2" "okd-network" {
+  name           = "okd-network"
   admin_state_up = "true"
 }
 
-resource "openstack_networking_subnet_v2" "subnet_1" {
-  name       = "sub-okd-lab-1"
-  network_id = "${openstack_networking_network_v2.network_1.id}"
+resource "openstack_networking_subnet_v2" "okd-subnet" {
+  name       = "okd-subnet"
+  network_id = "${openstack_networking_network_v2.okd-network.id}"
   cidr       = "192.168.199.0/24"
   ip_version = 4
 }
-resource "openstack_compute_secgroup_v2" "secgroup_1" {
-  name        = "secgroup_1"
-  description = "a security group"
-
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-}
 
 resource "openstack_networking_port_v2" "okd-master-port" {
-  count          = var.number_of_masters
-  name           = "okd-master-port_${count.index}"
+  name           = "okd-master-port-${count.index}"
   network_id         = "${openstack_networking_network_v2.network_1.id}"
-  admin_state_up = true
-  dns_name = "master-${count.index+1}.okd.lab"
+  admin_state_up     = "true"
   security_group_ids = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
-  
-  depends_on = [
-    openstack_networking_subnet_v2.subnet_1,
-  ]
-}
 
-resource "openstack_compute_instance_v2" "master" {
-  name            = "master-${count.index+1}"
-  count           = var.number_of_masters
-  flavor_name     = "Basic-1-1-10"
-  key_pair        = openstack_compute_keypair_v2.ssh.name
-  config_drive    = true
-  image_name      = openstack_images_image_v2.fedoracore.name
-  security_groups = ["${openstack_compute_secgroup_v2.secgroup_1.name}"]
-  network {
-#    port        = "${openstack_networking_port_v2.okd-master-port.*.id[count.index]}"
-    name = "${openstack_networking_network_v2.network_1.name}"
-	fixed_ip_v4 = "192.168.199.${9+1}"
+  fixed_ip {
+    "subnet_id"  = "${openstack_networking_subnet_v2.subnet_1.id}"
+    "ip_address" = [
+	"192.168.199.10",
+	"192.168.199.11"
+	"192.168.199.12"
+	"192.168.199.13"
+	"192.168.199.14"
+	"192.168.199.15"
+	]
   }
-}
-
-output "path_debug" {
-  value = "${var.number_of_workers+var.number_of_masters}"
 }
